11e56a99dde575032d02b9a9d626cc4762b39f4c
AKingJin <goahead_yingjun@163.com>
2016-04-19 16:31:25 +0800

    Initial Commit

---
+++ AKNetworkingEngine/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AKNetworkingEngine
+//
+//  Created by AKing on 16/4/19.
+//  Copyright © 2016年 AKing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
